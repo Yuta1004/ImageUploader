569171867f095c74c481dcb4ae503ea8fe44cb47
NakagamiYuta <guguru0014@gmail.com>
2023-02-16 06:45:24 +0900

    [add](front) 背景調整

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,12 +5,21 @@ import AlbumDetailPage from "./pages/AlbumDetail";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index         element={ <AlbumListPage/> }/>
-                <Route path="/album" element={ <AlbumDetailPage/> }/>
-            </Routes>
-        </BrowserRouter>
+        <div
+            style={{
+                width: "100vw",
+                height: "100vh",
+                background: "linear-gradient(45deg, #8bfdf3, #b884f7, #84fad6)",
+                backgroundSize: "200% 200%"
+            }} 
+        >
+            <BrowserRouter>
+                <Routes>
+                    <Route index         element={ <AlbumListPage/> }/>
+                    <Route path="/album" element={ <AlbumDetailPage/> }/>
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
 
