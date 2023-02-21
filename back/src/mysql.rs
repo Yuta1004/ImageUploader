1a14b280a6e46fcb9fff0c8383e1f97afa99aa28
NakagamiYuta <guguru0014@gmail.com>
2023-02-21 09:42:32 +0900

    [fix](back) CURRENT_TIMESTAMPにUTCが採用される問題を修正

--- back/src/mysql.rs
+++ back/src/mysql.rs
@@ -3,9 +3,16 @@ pub mod item;
 
 use diesel::mysql::MysqlConnection;
 use diesel::prelude::*;
+use diesel::sql_query;
 
 const ENDPOINT: &str = "mysql://root:mysql@mysql:3306/iuploader";
 
 fn create_connection() -> Result<MysqlConnection, ConnectionError> {
-    MysqlConnection::establish(ENDPOINT)
+    match MysqlConnection::establish(ENDPOINT) {
+        Ok(mut conn) => {
+            sql_query("SET time_zone = '+9:00';").execute(&mut conn).unwrap();
+            Ok(conn)
+        },
+        err => err
+    }
 }
